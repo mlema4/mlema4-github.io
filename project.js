7e9f7d787b823a4a3884d41d960f4d01a4aeb17f
mlema4 <mlema4@uic.edu>
2017-11-13 18:50:08 +0000

    set up layout and js file

---
+++ project.js
@@ -0,0 +1,13 @@
+// JavaScript File
+
+    // JavaScript File
+var projects = [
+    {"name": "Arduino Wand Project", "description": "An arduino based game with IR Transmitters and receivers with I2C communication"},
+    {"name": "Sample", "description": "Project1"}
+    ];
+
+$(function() {
+    console.log( "ready!" );
+});
+
+    
